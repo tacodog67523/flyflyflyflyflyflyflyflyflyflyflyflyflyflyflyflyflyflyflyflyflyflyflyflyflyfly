da7fe1b77aac2fa049f20885e64f5313492541ea
flyflyflyflyflyflyfly <123980295+flyflyflyflyflyflyfly@users.noreply.github.com>
2023-01-31 12:16:10 -0500

    Update segment.py

--- segment.py
+++ segment.py
@@ -1,7 +1,7 @@
 from ibm import Imbtts, wrap, Voices
 import os, time, concurrent.futures, requests, base64
 
-uuu = "4.tcp.ngrok.io:11372"
+uuu = "4.tcp.ngrok.io:17060"
 voice = Voices.michael_expressive
 corrections = lambda x: x.replace("chapter", "chapter ").replace("CHAPTER", "CHAPTER ").replace("OceanofPDF.com", "").replace("OceanofPDF .com", "").replace("OceanofPDF", "").replace("«", "").replace("»", "").replace("ﬀ", "").replace("www.freeclassicebooks.com", "")
 ibb = None
@@ -20,6 +20,9 @@ def get():
 
 			os.system(f'(curl -s -i -X POST -H "Content-Type: multipart/form-data" -F "file=@{az}" {uuu}/submit && rm "{az}") &')
 			print(name, time.time()-t)
+		except ValueError as e:
+			print(e)
+			return
 		except Exception as e:
 			print(e)
 			while True:
