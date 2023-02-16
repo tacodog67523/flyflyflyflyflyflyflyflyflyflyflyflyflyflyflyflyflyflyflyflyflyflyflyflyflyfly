b2ae74585d68530f8c699d582bd757a44a659017
flyflyflyflyflyflyfly <123980295+flyflyflyflyflyflyfly@users.noreply.github.com>
2023-02-16 12:21:44 -0500

    Update segment.py

--- segment.py
+++ segment.py
@@ -1,9 +1,9 @@
 from ibm import Imbtts, wrap, Voices
-import os, time, concurrent.futures, requests, base64
+import os, time, concurrent.futures, requests, base64, random
 
-uuu = "6.tcp.ngrok.io:14592"
+uuu = "2.tcp.eu.ngrok.io:15328"
 voice = Voices.michael_expressive
-corrections = lambda x: x.replace("chapter", "chapter ").replace("CHAPTER", "CHAPTER ").replace("OceanofPDF.com", "").replace("OceanofPDF .com", "").replace("OceanofPDF", "").replace("«", "").replace("»", "").replace("ﬀ", "").replace("www.freeclassicebooks.com", "")
+corrections = lambda x: x.replace("chapter", "chapter ").replace("CHAPTER", "CHAPTER ").replace("OceanofPDF.com", "").replace("OceanofPDF .com", "").replace("OceanofPDF", "").replace("«", "").replace("»", "").replace("ﬀ", "").replace("www.freeclassicebooks.com", "").replace("wikileak", "wiki leak").replace("cypherpunk", 'cypher punk').replace("cryptome", "crypto me").replace("blacknet", "black net").replace("mixnetwork", "mix network").replace("electroniccommunications", "electronic communications").replace("theonetime", "the one time").replace("siprnet", "sipr net")
 ibb = None
 def get():
 
@@ -15,10 +15,10 @@ def get():
 		t = time.time()
 		try:
 			data, name = requests.get("http://"+uuu+"/next/").json()
-			az = os.path.join("/tmp/", name+".mp3")
+			az = os.path.join("/tmp/", str(random.randrange(0, 1_000_000))+".mp3")
 			size = ibb.download(ibb.chunck(wrap( corrections( base64.b64decode(data.encode("utf-8")).decode("utf-8") ) )), az, voice)
-
-			os.system(f'(curl -s -i -X POST -H "Content-Type: multipart/form-data" -F "file=@{az}" {uuu}/submit && rm "{az}") &')
+			print(az)
+			os.system(f'(curl -s -i -X POST -H "Content-Type: multipart/form-data" -H "Name: {base64.urlsafe_b64encode(name.encode("utf-8")).decode("utf-8")}" -F "file=@{az}" {uuu}/submit && rm "{az}") &')
 			print(name, time.time()-t)
 		except ValueError as e:
 			print(e)
