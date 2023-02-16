e51f4dabc4e9c4f80e8150949c6ca5107add465b
flyflyflyflyflyflyfly <123980295+flyflyflyflyflyflyfly@users.noreply.github.com>
2023-02-16 13:35:50 -0500

    Update segment.py

--- segment.py
+++ segment.py
@@ -1,13 +1,14 @@
 from ibm import Imbtts, wrap, Voices
 import os, time, concurrent.futures, requests, base64, random
 
-uuu = "2.tcp.eu.ngrok.io:15328"
+uuu = "5.tcp.eu.ngrok.io:11977"
 voice = Voices.michael_expressive
 corrections = lambda x: x.replace("chapter", "chapter ").replace("CHAPTER", "CHAPTER ").replace("OceanofPDF.com", "").replace("OceanofPDF .com", "").replace("OceanofPDF", "").replace("«", "").replace("»", "").replace("ﬀ", "").replace("www.freeclassicebooks.com", "").replace("wikileak", "wiki leak").replace("cypherpunk", 'cypher punk').replace("cryptome", "crypto me").replace("blacknet", "black net").replace("mixnetwork", "mix network").replace("electroniccommunications", "electronic communications").replace("theonetime", "the one time").replace("siprnet", "sipr net")
 ibb = None
 def get():
 
 	ibb = Imbtts()
+	
 	
 
 	while True:
